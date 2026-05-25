3bbf4abe6e27e3b3c5af734f34aa33f5606f4913
Mumuzinho <mumuzinho.moltbot@gmail.com>
2026-05-25 23:31:56 +0000

    tweak(pwa): ícone um pouco maior + versionar URLs pra furar cache iOS

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,7 @@
 
 // Web Push Service Worker (PWA)
 
-const SHELL_CACHE = 'nicco-shell-v9';
+const SHELL_CACHE = 'nicco-shell-v10';
 
 async function cacheShell() {
   const cache = await caches.open(SHELL_CACHE);
